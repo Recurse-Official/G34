2a84074898ad67461ac0c1a256c90b00f76ecd89
= <=>
2024-11-30 18:25:43 +0530

    OAuth Login Implemented

---
+++ Backend/BackendSpring/src/test/java/com/example/financemanager/FinanceManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
